f431ea867f83f7e5ad58f6b94bd7695c6720cd1d
Ashish Shubham <ashish.shubham@thoughtspot.com>
2020-06-10 21:58:41 -0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import useFetch from 'use-http';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const resp = useFetch('https://google.com');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
